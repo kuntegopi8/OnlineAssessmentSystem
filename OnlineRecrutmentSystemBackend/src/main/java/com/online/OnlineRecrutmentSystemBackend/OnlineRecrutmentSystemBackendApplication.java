f1f6a9b8aa8f87d17287f408c70e17b25fe2f28e
gopi <kuntegopi8@gmail.com>
2026-07-29 10:57:15 +0530

    Initial commit

---
+++ OnlineRecrutmentSystemBackend/src/main/java/com/online/OnlineRecrutmentSystemBackend/OnlineRecrutmentSystemBackendApplication.java
@@ -0,0 +1,13 @@
+package com.online.OnlineRecrutmentSystemBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineRecrutmentSystemBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineRecrutmentSystemBackendApplication.class, args);
+	}
+
+}
